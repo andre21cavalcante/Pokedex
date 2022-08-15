1b14c935a8f1cc664885bedf000958efaad0a65c
Andre Cavalcante <andre.cavalcantejr21@gmail.com>
2022-08-15 04:12:59 -0300

    Criando base da pokedex e instalando o styled component

--- poke/src/Components/Pokemon.js
+++ poke/src/Components/Pokemon.js
@@ -1,25 +1,9 @@
 import React, { useState, useEffect} from 'react';
-import { Api } from './Services/Api';
-import * as S from "./Pokemon.js"
 
 const Pokemon = () =>{
-    const [pokemon, setPokemon] = useState([]);
-
-    const [select, setSelect] = useState("");
-
-    useEffect(() =>{
-        Api.get("").then((response) => {
-            setPokemon(response.data);
-            console.log(response.data);
-        });
-    }, []);
-
   return (
-    <S.Main>
         <div>
-        
         </div>
-    </S.Main>
   )
 }
 
